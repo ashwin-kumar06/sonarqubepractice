3b091e6d8c4a0acf0638f4f377e60e62eadc0175
ashwin <ashwinkumar0850@gmail.com>
2024-01-22 09:18:21 +0530

    first

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+class Program{
+  public static void Main(string[] args){
+    string sender = "ashwinkumar0850@gmail.com";
+    string senderPass = "tmlr cofs gohl lapn";
+    string recieve = "aswinkumar0850@gmail.com";
+
+    MailMessage mail = new MailMessage(sender,recieve);
+    mail.Subject = "Subject";
+    mail.Body = "Hi this is ashwin";
+
+    SmtpClient smtpClient= new SmtpClient("smtp.gmail.com");
+    smtpClient.Port = 587;
+    smtpClient.Credentials = new NetworkCredential(sender,senderPass);
+    smtpClient.EnableSsl = true;
+
+    try{
+      smtpClient.Send(mail);
+      Console.WriteLine("Sent Successfully");
+    }
+    catch (Exception e){
+      Console.WriteLine($"Error: {e.Message}");
+    }
+  }
+}
